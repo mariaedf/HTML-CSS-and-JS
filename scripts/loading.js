0f878762be3ec4e4ebe3254d1b0fc68bbb2bfc5e
mariaedf <mariaedf001@gmail.com>
2025-03-14 09:51:16 -0300

    Adding dots

---
+++ scripts/loading.js
@@ -0,0 +1,6 @@
+window.onload = function () {//page loading
+  setTimeout(function () {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("body").style.overflow = "scroll";
+  }, 1000);
+};
